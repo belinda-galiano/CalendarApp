a219ea0af3fe23e491f952cff4cec7333295875d
Belinda Galiano <belindagalianomolina@gmail.com>
2018-11-23 15:38:47 -0700

    added eslint update version, added the basic html to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Calendar App</h1>
+        <h1> Get a name day for a specific day</h1>
+        <h3>Pick a day</h3>
+        <div>
+          <p>calendar</p>
+        </div>
+        <h3>Here is goin to appear the name</h3>
+        <div>
+          <p>name-text-box</p>
+        </div>
       </div>
     );
   }
