40acd34331461c019695409f536a9c5080de5f3d
Belinda Galiano <belindagalianomolina@gmail.com>
2018-11-23 20:00:02 -0700

    Lifted State Up Calendar to App,added more data to data.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import Calendar from './Calendar';
+import moment from 'moment';
+
+const apiURL = 'data.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: '',
+      day: 0,
+      month: 0,
+      startDate: moment()
+    }
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  componentDidMount() {
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(json => this.setState({
+        name: json.data[1].name,
+        day: json.data.day,
+        month: json.data.month
+      }))
+  };
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
   }
+
   render() {
+    const { name, startDate } = this.state;
+    console.log(name);
     return (
       <div>
         <h1 className='tilte'> Get a name day for a specific day</h1>
         <div className='main-content'>
           <div className='content-box'>
             <h3>Pick a day</h3>
-            <Calendar />
+            <Calendar startDate={startDate} onChange={(date) => this.handleChange(date)} />
           </div>
           <div className='content-box'>
             <h3>Here is goin to appear the name</h3>
-            <div className='name-box'>Name</div>
+            <div className='name-box'>{name}</div>
           </div>
         </div>
       </div>
--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import ModernDatepicker from 'react-modern-datepicker';
-import moment from 'moment';
 import './Calendar.css';
 
 class Calendar extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      startDate: moment()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
-  }
-
   render() {
     return <ModernDatepicker 
-          date={this.state.startDate} 
+          date={this.props.startDate} 
           format={'DD-MM-YYYY'} 
-          // showBorder
           className='style'         
-          onChange={(date) => this.handleChange(date)}
+          onChange={this.props.onChange}
           placeholder={'Select a date'}
         />
   }
